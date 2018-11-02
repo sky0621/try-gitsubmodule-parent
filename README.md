ab08e21ae1192b3efd66ec77fcb3d570eb546a45
sky0621 <sky14work@gmail.com>
2018-11-02 09:25:49 +0900

    try

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # try-gitsubmodule-parent
+
+$ mvn archetype:generate -Dappengine-version=1.9.67 -Djava8=true -Dfilter=com.google.appengine.archetypes:
+6
+
---
+++ try-gitsubmodule-parent/src/main/java/gitsubmodule/parent/HelloGreeting.java
@@ -0,0 +1,20 @@
+package gitsubmodule.parent;
+
+public class HelloGreeting {
+
+  public String message;
+
+  public HelloGreeting() {};
+
+  public HelloGreeting(String message) {
+    this.message = message;
+  }
+
+  public String getMessage() {
+    return message;
+  }
+
+  public void setMessage(String message) {
+    this.message = message;
+  }
+}
